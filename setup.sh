a2d53e0834b48938ea87866aa339a377f6aa2af5
Florent Clarret <florent.clarret@gmail.com>
2019-02-13 23:19:52 +0100

    chore(setup): improve the script

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 
 VENV=venv
 
-if [ ! -d "$VENV" ]
+if [[ ! -d "$VENV" ]]
 then
 
     PYTHON=`which python3`
 
-    if [ ! -f $PYTHON ]
+    if [ ! -f ${PYTHON} ]
     then
         echo "could not find python"
     fi
-    virtualenv -p $PYTHON $VENV
+    virtualenv -p ${PYTHON} ${VENV}
 
 fi
 
-. $VENV/bin/activate
+. ${VENV}/bin/activate
 
 pip install -r requirements.txt
